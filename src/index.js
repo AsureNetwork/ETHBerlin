696ef93179e54ccfa527cb0923a3a9c67e977cd0
Fabian Raetz <fabian.raetz@gmail.com>
2018-09-08 23:28:30 +0200

    redeploy

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router'
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { DrizzleProvider } from 'drizzle-react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
