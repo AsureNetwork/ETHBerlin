dec8eaf9c6c1774cbcda4552fc676366294d7867
Paul Mizel <email@pmizel.de>
2018-09-09 00:28:07 +0200

    init logos

--- src/layouts/home/Home.js
+++ src/layouts/home/Home.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Button, Col, Row, Jumbotron } from 'reactstrap';
+import ETHBerlinLogo from './../../content/ethberlin_logo.png';
+
 
 class Home extends Component {
     render() {
@@ -7,7 +9,10 @@ class Home extends Component {
             <Row>
                 <Col xs="12">
                     <Jumbotron>
-                        <h1 className="display-3">awd, world!</h1>
+                        <img src={ETHBerlinLogo} width="320px" />
+                        <h1 className="display-3">
+                            awd, decentralized pension world!
+                        </h1>
                         <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling
                             extra
                             attention to featured content or information.</p>
